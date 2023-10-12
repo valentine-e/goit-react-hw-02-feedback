b9c3ee4e64b2241bc037ecd428c38a562135c7ec
valentine-e <valia101112@gmail.com>
2023-10-12 23:37:22 +0300

    first markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import Feedback from './Feedback/Feedback.jsx';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +10,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Feedback />
     </div>
   );
 };
